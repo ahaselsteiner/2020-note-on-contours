0b68a7312415a2ae98e29ffbd9a24d33b6d7b0a0
Andreas <a.haselsteiner@uni-bremen.de>
2020-03-09 19:47:40 +0100

    Change response function to have positive angles.

--- directional stuff/directionalResponse.m
+++ directional stuff/directionalResponse.m
@@ -1,8 +1,8 @@
 function r = directionalResponse(hx, hy)
-    theta = -310 / 180 * pi;
+    phi = 310 / 180 * pi;
     a = 1.3;
     b = 5;
-    hxPrime = hx * cos(theta) - hy * sin(theta);
-    hyPrime = hx * sin(theta) + hy * cos(theta);
+    hxPrime = hx * cos(phi) + hy * sin(phi);
+    hyPrime = -1 * hx * sin(phi) + hy * cos(phi);
     r = sqrt(a * hxPrime.^2 + b * hyPrime.^2);
 end
